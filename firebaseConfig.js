6204c34724c219d16fa67c0093ef2a352befebc5
May <142784317+May-ag0@users.noreply.github.com>
2025-10-09 14:10:56 +0200

    ...

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import firebaseConfig from "./firebaseEnv";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+export const getStorageRef = (path) => ref(storage, path);
+export const getDownloadUrl = async (path) => {
+  const storageRef = getStorageRef(path);
+  const downloadUrl = await getDownloadURL(storageRef);
+  return downloadUrl;
+};
